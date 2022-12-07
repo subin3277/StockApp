acf013a296301e8e6f5ec4813d7820d8382fc4ef
seoda0000 <seoda0000@gmail.com>
2022-12-07 14:10:18 +0900

    activity build

--- app/src/main/java/com/example/stockapp/LoginRequest.java
+++ app/src/main/java/com/example/stockapp/LoginRequest.java
@@ -1,7 +1,5 @@
 package com.example.stockapp;
 
-import androidx.annotation.Nullable;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
@@ -9,20 +7,18 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterRequest extends StringRequest {
+public class LoginRequest extends StringRequest {
 
     // 서버 URL 설정
-    final static private String URL = "http://13.124.21.50:8080/api/swagger-ui.html/user/join/";
+    final static private String URL = "http://13.124.21.50:8080/api/swagger-ui.html/user/login/";
     private Map<String, String> map;
 
-    public RegisterRequest(String userID, String userPassword, String userName, String userEmail, Response.Listener<String> listener) {
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         map = new HashMap<>();
         map.put("userID", userID);
         map.put("userPassword", userPassword);
-        map.put("userName", userName);
-        map.put("userEmail", userEmail);
     }
 
     @Override
--- app/src/main/java/com/example/stockapp/RegisterActivity.java
+++ app/src/main/java/com/example/stockapp/RegisterActivity.java
@@ -3,12 +3,47 @@ package com.example.stockapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText et_id, et_pass, et_name, et_email;
+    private Button btn_register;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // 액티비티 시작시 처음으로 실행되는 생명주기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        et_name = findViewById(R.id.et_name);
+        et_email = findViewById(R.id.et_email);
+        btn_register = findViewById(R.id.btn_register);
+
+        // 회원가입 버튼 클릭 시 수행
+        btn_register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // EditText에 현재 입력되어 있는 값을 가져온다.
+                String userID = et_id.getText().toString();
+                String userPass = et_pass.getText().toString();
+                String userName = et_name.getText().toString();
+                String userEmail = et_email.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        
+                    }
+                };
+
+            }
+        });
     }
 }
