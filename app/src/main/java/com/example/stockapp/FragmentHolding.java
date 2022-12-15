b1519de87c2a850b1c06e6bd645eb46fb871ebf9
seoda0000 <seoda0000@gmail.com>
2022-12-15 18:27:34 +0900

    fragment

---
+++ app/src/main/java/com/example/stockapp/FragmentHolding.java
@@ -0,0 +1,24 @@
+package com.example.stockapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentHolding extends Fragment {
+    public FragmentHolding() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_holding, container, false);
+    }
+
+
+}
--- app/src/main/java/com/example/stockapp/MainActivity.java
+++ app/src/main/java/com/example/stockapp/MainActivity.java
@@ -1,46 +1,50 @@
 package com.example.stockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText id, pwd;
-    Button login_btn;
+//    EditText id, pwd;
+//    Button login_btn;
+    Button btn_wishlist, btn_holding;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_wishlist = (Button) findViewById(R.id.btn_wishlist);
+        btn_holding = (Button) findViewById(R.id.btn_holding);
+
+        btn_wishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                FragmentWishlist fragmentWishlist = new FragmentWishlist();
+                transaction.replace(R.id.frame, fragmentWishlist);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        btn_holding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                FragmentHolding fragmentHolding = new FragmentHolding();
+                transaction.replace(R.id.frame, fragmentHolding);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
 
 
     }
