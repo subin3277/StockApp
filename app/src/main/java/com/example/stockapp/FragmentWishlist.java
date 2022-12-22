70198fa32c2bd7020594323a1bd7c3661dfe363b
seoda0000 <seoda0000@gmail.com>
2022-12-22 17:06:40 +0900

    221222

--- app/src/main/java/com/example/stockapp/FragmentWishlist.java
+++ app/src/main/java/com/example/stockapp/FragmentWishlist.java
@@ -12,6 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+
+import java.lang.reflect.Array;
+
 public class FragmentWishlist extends Fragment {
     private TextView test;
     private String obj;
@@ -30,6 +34,11 @@ public class FragmentWishlist extends Fragment {
         {
             obj = getArguments().getString("obj_wishlist"); // 프래그먼트1에서 받아온 값 넣기
             test.setText(obj);
+//            Gson gson = new Gson();
+//            Array obj_wishlist = gson.fromJson(obj, Array.class);
+//            for(int i=0; i < 3 ; i ++ ){
+//                // obj_wishlist[i];
+//            }
         }
         return view;
     }
